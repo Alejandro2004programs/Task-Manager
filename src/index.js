70aec5ae92c2768438374c00d54f7ee2643cb7c9
Alejandro Alvarez <ala007@ucsd.edu>
2025-03-29 10:47:02 -0700

    added classes for creating new project and todos. and started the displayController object

--- src/index.js
+++ src/index.js
@@ -1,2 +1,50 @@
 import "./styles.css";
 
+console.log("testing");
+
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.dataId = crypto.randomUUID();
+    }
+}
+
+class Project {
+    constructor(projectName) {
+        this.projectName = projectName;
+        this.todoList = [];
+        this.dataId = crypto.randomUUID();
+    }
+}
+
+const todoListController = function() {
+
+    return {};
+}();
+
+
+// First worry about getting projects to work and show on display, then to-do's
+const projectsController = function() {
+    const newProject = function(projectName) {
+        const project = new Project(projectName);
+        return project;
+    }
+    return {newProject};
+}();
+
+// Change to displayProjectsController
+const displayController = function() {
+    const projectsContainer = document.querySelector(".projectsContainer");
+    const addProject = function(project) {
+        const newProject = document.createElement("div");
+        newProject.setAttribute("class", project.dataId);
+        newProject.textContent = project.projectName;
+        projectsContainer.appendChild(newProject);
+    } 
+    return {addProject};
+} ();
+
+// Add a todoListDisplayController
