882435bdcd37b7d0c8c590a43dfc3ca35269ba88
Alejandro Alvarez <ala007@ucsd.edu>
2025-06-20 13:33:26 -0700

    starting this project all over again with a different UI

--- src/index.js
+++ src/index.js
@@ -19,37 +19,3 @@ class Project {
         this.dataId = crypto.randomUUID();
     }
 }
-
-const todoListLogic = function() {
-
-    return {};
-}();
-
-
-// First worry about getting projects to work and show on display, then to-do's
-const projectsLogic = function() {
-    const newProject = function(projectName) {
-        const project = new Project(projectName);
-        return project;
-    }
-    return {newProject};
-}();
-
-const displayProjects = function() {
-    const projectsContainer = document.querySelector(".projectsContainer");
-    const addProject = function(project) {
-        const newProject = document.createElement("div");
-        newProject.setAttribute("class", project.dataId);
-        newProject.setAttribute("class", ".project")
-        newProject.textContent = project.projectName;
-        projectsContainer.appendChild(newProject);
-    } 
-    return {addProject};
-} ();
-
-const displayTodoLists = function() {
-
-} ();
-
-
-displayProjects.addProject("Testing title");
